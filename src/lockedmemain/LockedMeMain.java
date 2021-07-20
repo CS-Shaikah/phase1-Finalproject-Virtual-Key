2c803853f150fd9da6f5a7901d57ef5330441633
shaikah <shaikah--@hotmail.com>
2021-07-20 03:39:40 +0300

    phase1-Finalproject-Virtual-Key

---
+++ src/lockedmemain/LockedMeMain.java
@@ -0,0 +1,21 @@
+
+package lockedmemain;
+
+/**
+ *
+ * @author shaikah almoqbel
+ */
+public class LockedMeMain {
+
+    public static void main(String[] args) {
+        
+        // Create "main" folder if not present in current folder structure
+		FileOperations.createMainFolderIfNotPresent("main");
+		
+		MenuOptions.printWelcomeScreen("LockedMe", "shaikah almoqbel");
+		
+		HandleOptions.handleWelcomeScreenInput();
+	}
+    }
+    
+
